d90b1a161ba6ac04255f67b453e11d784f0d1ad1
ksilva <klara.tsilva@gmail.com>
2025-06-03 17:11:33 +0200

    Add new and edit button

--- app/[locale]/accounts/[id]/page.tsx
+++ app/[locale]/accounts/[id]/page.tsx
@@ -1,7 +1,28 @@
 import React from "react";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
 
-const AccountDetailPage = () => {
-  return <div>AccountDetailPage</div>;
+interface Props {
+  params: {
+    locale: string;
+    id: string;
+  };
+}
+
+const AccountDetailPage = async ({ params }: Props) => {
+  const { id, locale } = params;
+
+  console.log(id, "id");
+  console.log(locale, "locale");
+
+  return (
+    <div>
+      <h1>Account Detail Page</h1>
+      <Link href={`/${locale}/accounts/${id}/edit`}>
+        <Button>Edit Account</Button>
+      </Link>
+    </div>
+  );
 };
 
 export default AccountDetailPage;
--- app/[locale]/accounts/page.tsx
+++ app/[locale]/accounts/page.tsx
@@ -1,7 +1,16 @@
 import React from "react";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
 
 const Accounts = () => {
-  return <div>Accounts</div>;
+  return (
+    <div>
+      <h1>Accounts</h1>
+      <Link href="/accounts/new">
+        <Button className="btn-primary">Add New Account</Button>
+      </Link>
+    </div>
+  );
 };
 
 export default Accounts;
