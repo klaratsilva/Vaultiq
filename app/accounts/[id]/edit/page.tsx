d6c0caa8123c6f6647d540a3c2a84e294dab95c3
ksilva <klara.tsilva@gmail.com>
2025-06-03 09:49:18 +0200

    Create app routes

---
+++ app/accounts/[id]/edit/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const EditAccountDetails = () => {
+  return <div>EditAccountDetails</div>;
+};
+
+export default EditAccountDetails;
---
+++ app/accounts/new/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AddNewAccount = () => {
+  return <div>AddNewAccount</div>;
+};
+
+export default AddNewAccount;
